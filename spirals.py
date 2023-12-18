ef803f75b4254fe9de4572802b11b4bcd607d705
Helmstedter <Russell.Helmstedter@venturausd.org>
2023-12-18 12:02:42 -0800

    added more spirals

--- spirals.py
+++ spirals.py
@@ -11,7 +11,7 @@ def hex_int(num: int) -> str:
 
 
 class hex_color:
-    """Looping color that returns hex value for str"""
+    """Looping color that returns hex value for str."""
 
     def __init__(self, colors: list[int] = [0, 0, 0]) -> None:
         if len(colors) > 3:
@@ -86,7 +86,7 @@ def sipral1(turt: turtle.Turtle, screen: turtle.Screen) -> None:
             break
 
 
-def logan(turt: turtle.Turtle, screen: turtle.Screen) -> None:
+def logan(turt: turtle.Turtle) -> None:
     # Begin!
     turt.speed(0)
     turt.pensize(1)
@@ -112,15 +112,14 @@ def logan(turt: turtle.Turtle, screen: turtle.Screen) -> None:
             turt.color(str(color))
 
 
-def zeb(turt, screen) -> None:
+def zeb(turt: turtle.Turtle, screen) -> None:
     screen.bgcolor("#FF072F")
-
     # Begin!
     turt.pensize(3)
     turt.speed(0)
-    turt.color("#65FF65")
+    turt.color("#fabc02")
     turt.penup()
-    turt.setposition(-100,0)
+    turt.setposition(-100, 0)
     turt.pendown()
 
     sides = cycle([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9])
@@ -139,7 +138,7 @@ def nick(turt, screen) -> None:
     turt.pensize(3)
 
     turt.penup()
-    turt.setposition(-50,100)
+    turt.setposition(-50, 100)
     turt.pendown()
     sides = cycle([2, 4, 1, 3, 2, 4, 3, 5, 4, 6])
     colors = cycle(["#FF12A4", "#12FFD6", "#4DB28B", "#E41B42", "#16E946"])
@@ -153,12 +152,11 @@ def nick(turt, screen) -> None:
         if x_start == round(x, 0) and y_start == round(y, 0) and turt.heading() == 0:
             break
 
-
 def samuel(turt, screen) -> None:
     # Begin!
     turt.pensize(2)
     turt.penup()
-    turt.setposition(0,-200)
+    turt.setposition(0, -200)
     turt.pendown()
     sides = cycle([2, 6, 4, 4, 10])
     colors = cycle(["#99D5C9", "#6C969D", "#645E9D", "#726DA8", "#8661C1"])
@@ -178,7 +176,7 @@ def cynthia_velazquez(turt, screen) -> None:
     # Begin!
     turt.pensize(4)
     turt.penup()
-    turt.setposition(0,200)
+    turt.setposition(0, 200)
     turt.pendown()
     sides = cycle([9, 5, 7, 3, 5])
     colors = cycle(["#5DA5F7", "#379F56", "#5471E3", "#26C5B1", "#8DC59A"])
@@ -218,7 +216,7 @@ def tim(turt, screen):
     turt.pensize(2)
     turt.speed(0)
     turt.penup()
-    turt.setposition(0,200)
+    turt.setposition(0, 200)
     turt.pendown()
     sides = cycle([5, 3, 7, 4.4, 6, 1, 9])
     colors = cycle(["#6c676b", "#090e27", "#6c676b", "#090e27"])
@@ -240,7 +238,7 @@ def sai(turt, screen):
     colors = cycle(["#335C67", "#FFF3B0", "#E09F3E", "#9E2A2B", "#540B0E"])
 
     turt.penup()
-    turt.setposition(-100, -300)
+    turt.setposition(0, -200)
     turt.pendown()
     x_start, y_start = turt.position()
     while True:
@@ -260,7 +258,7 @@ def ethan_meyer(turt, screen):
     turt.speed(0)
     turt.shape("turtle")
     turt.penup()
-    turt.setpos(50, 200)
+    turt.setpos(0, 300)
     turt.pendown()
     sides = cycle([5, 3, 7, 4.4, 6])
     x_start, y_start = turt.position()
@@ -298,6 +296,9 @@ def grace(turt, screen):
     # Begin!
     turt.pensize(5)
     turt.speed(0)
+    turt.penup()
+    turt.setpos(-200, 0)
+    turt.pendown()
     sides = cycle([5, 2, 3, 6, 2, 1, 1, 5, 3, 6])
     colors = cycle(["#FFCDB2", "#FFB4A2", "#E5989B", "#B5838D", "#6D6875"])
     x_start, y_start = turt.position()
@@ -357,20 +358,146 @@ def jake(turt, screen):
             break
 
 
+def ethan_nobel(turt, screen):
+    screen.bgcolor("#5F4BB6")
+
+    # Begin!
+    turt.shape("circle")
+    turt.color("#FE621D")
+    turt.pensize(7)
+    turt.speed(0)
+    turt.penup()
+    turt.setposition(-100, 200)
+    turt.pendown()
+
+    sides = cycle([1, 1, 5, 5, 9, 9, 13, 13, 17, 17])
+    x_start, y_start = turt.pos()
+    while True:
+        side = next(sides)
+        turt.forward(5 * side)
+        turt.left(110)
+        x, y = turt.pos()
+        if x_start == round(x, 0) and y_start == round(y, 0):
+            break
+
+
+def ethan_nobel2(turt, screen):
+    screen.bgcolor("#c30022")
+    screen.tracer(0, 0)
+    # Begin!
+    turt.shape("turtle")
+    turt.speed(0)
+    turt.color("#00a53c")
+    turt.circle(0)
+    turt.pensize(2)
+
+    def draw_spiral(x, y):
+        turt.penup()
+        turt.setposition(x, y)
+        turt.pendown()
+        sides = 3
+        distance = 3
+        for side in range(300):
+            turt.forward(distance)
+            turt.left(360 / sides + 1)
+            distance += 2
+            if side % 2 == 0:
+                screen.update()
+
+    draw_spiral(200, 0)
+    draw_spiral(-200, 0)
+    draw_spiral(0, -200)
+    draw_spiral(0, 200)
+    draw_spiral(250, 250)
+    draw_spiral(-250, 250)
+    draw_spiral(250, -250)
+    draw_spiral(-250, -250)
+    screen.tracer(1, 1)
+
+
+def sanvi(turt, screen):
+    screen.bgcolor("#BFFFA9")
+
+    # Begin!
+    turt.shape("turtle")
+    turt.speed(0)
+    turt.pensize(10)
+
+    colors = ["#386641", "#6a994e", "#a7c957", "#da2c38", "#f2e8cf"]
+
+    sides = 3.5
+    distance = 40
+
+    turt.penup()
+    turt.setposition(-30, 0)
+    turt.pendown()
+
+    for _ in range(30):
+        for color in colors:
+            turt.color(color)
+            turt.forward(distance)
+            turt.left(360 / sides + 0.01)
+            distance += 3
+
+
+def maya(turt, screen):
+    turt.speed(0)
+    turt.pensize(3)
+    turt.penup()
+    turt.setposition(50, 300)
+    turt.pendown()
+    sides = cycle([1, 2, 3, 4, 5])
+    colors = cycle(["#3D348B", "#FA003F", "#7678ED", "#F7B801", "#F18701", "#F35B04"])
+    x_start, y_start = turt.position()
+    while True:
+        side = next(sides)
+        turt.color(next(colors))
+        turt.forward(50 * side)
+        turt.left(322)
+        x, y = turt.position()
+        if x_start == round(x, 0) and y_start == round(y, 0):
+            break
+
+
+def jack(turt, screen):
+    turt.speed(0)
+    screen.tracer(0, 0)
+    sides = cycle([9, 7, 6, 2, 8, 3, 3, 4, 6, 5, 3, 11, 4])
+    colors = cycle(["red", "orange", "yellow", "green", "blue", "purple"])
+    x_start, y_start = turt.position()
+    counter = 0
+    while True:
+        turt.color(next(colors))
+        side = next(sides)
+        turt.forward(30 * side)
+        turt.left(69)
+        x, y = turt.position()
+        if x_start == round(x, 0) and y_start == round(y, 0):
+            break
+        if counter % 2 == 0:
+            screen.update()
+    screen.update()
+    screen.tracer(1, 1)
+
+
 # all the funcs to run if you add one make sure to put it here
 funcs = [
-    # sipral1,
     # logan,
-    zeb,
-    nick,
-    samuel,
-    cynthia_velazquez,
-    xoe,
-    tim,
-    sai,
-    ethan_meyer,
-    yas,
-    grace,
-    joel,
-    jake,
+    # zeb,
+    # nick,
+    # samuel,
+    # cynthia_velazquez,
+    # xoe,
+    # tim,
+    # sai,
+    # ethan_meyer,
+    # yas,
+    # grace,
+    # joel,
+    # jake,
+    # ethan_nobel,
+    # ethan_nobel2,
+    # sanvi,
+    # maya,
+    jack,
 ]
